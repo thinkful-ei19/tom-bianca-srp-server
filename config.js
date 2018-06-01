d684b4f2ab70cfb742ab6a275d9d7ab08ec81d1f
thaborak <thaborak@gmail.com>
2018-06-01 16:05:24 -0400

    :rocket:

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://learn-high-valyrian.netlify.com/login',
   DATABASE_URL:
         process.env.DATABASE_URL || 'mongodb://dev:dev@ds237660.mlab.com:37660/tom-bianca-srp',
   TEST_DATABASE_URL:
